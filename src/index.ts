107632c11806ba3ab6283d09ac6bd7016c228279
Damien Springuel <dspringuel@vendasta.com>
2019-04-22 07:54:30 -0600

    event source server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log(`Hello world!`);
+import express, { NextFunction, Express, Request, Response } from 'express';
+
+const port = 13000;
+
+const app: Express = express();
+
+app.use(function(req: Request, response: Response, next: NextFunction) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/stream', (request: Request, response: Response) => {
+  console.log(`Client opened the connection.`);
+  response.header("Content-Type", "text/event-stream");
+  response.header("Connection", "keep-alive");
+
+  response.write('data: Hello World!\n\n');
+
+  let i = 1;
+  const stream = () => {
+    if (i > 10) {
+      response.write(`event: streamEnd\n`);
+      response.write(`data: Stream ended\n\n`);
+    }
+    response.write(`data: Stream ${i}\n\n`)
+    i += 1;
+    setTimeout(stream, 500);
+  }
+  stream();
+
+  request.on('close', () => {
+    console.log(`Client closed the connection.\n`);
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server listening to ${port}`);
+});
